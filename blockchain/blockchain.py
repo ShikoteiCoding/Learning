a35caec614bf82541472def35f72c3efa308b651
Come Vitis <business.vitis@gmail.com>
2022-07-06 17:21:53 +0200

    Adding a tailblock as a special case of block

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -4,24 +4,32 @@ from datetime import datetime
 @dataclass
 class Data:
     """ Data contained in a block. """
-    timestamp: datetime
-    proof: str
-    signature: str
+    timestamp: datetime = field(init=True, default_factory=datetime.now)
+    transactions: str = field(init=True, default="")
 
 @dataclass
 class Block:
     """ Block storing class. """
 
-    data: dict = field(init=True)
+    data: Data = field(init=True)
     signature: str = field(init=True)
     prev: 'Block' = field(init=True)
+    next: None | 'Block' = field(init=False, default=None)
+
+@dataclass
+class TailBlock(Block):
+    """ Tail block. """
+
+    data: Data = field(init=False, default_factory=Data)
+    signature: str = field(init=False, default="")
+    prev: None = field(init=False, default=None)
     next: None | 'Block' = field(init=False, default=None)
 
 @dataclass
 class Blockchain:
     """ Chain storing class. """
 
-    tail: Block = field(init=True)
+    tail: TailBlock = field(init=True)
     head: Block = field(init=False)
     _size: int = field(init=False, default=0)
 
---
+++ blockchain/main.py
@@ -0,0 +1,12 @@
+from .blockchain import Blockchain, Block, Data, TailBlock
+
+def create_block_chain_without_users():
+    """ Create and build blockchain without users. Simplistic scenario. """
+
+    fb = TailBlock()
+    bc = Blockchain
+
+
+
+if __name__ == "__main__":
+    create_block_chain_without_users()
