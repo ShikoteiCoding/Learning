7b238f49395bc402da234eb37f1160cc0766fa57
Come Vitis <business.vitis@famoco.com>
2022-07-06 05:38:03 +0200

    Enriching with more classes

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,9 +1,27 @@
+from dataclasses import dataclass, field
+from datetime import datetime
 
+@dataclass
+class Data:
+    """ Data contained in a block. """
 
-class blockchain:
+    timestamp: datetime
+    proof: str
+    signature: str
 
-    def __init__(self):
-        raise NotImplementedError()
+@dataclass
+class Block:
+    """ Block storing class. """
+
+    prev: None | 'Block' = field(init=True)
+    next: None | 'Block' = field(init=False, default=None)
+    data: dict = field(init=False, default_factory=dict)
+
+@dataclass
+class Blockchain:
+    """ Chain storing class. """
+
+    current_block: Block = field(init=True)
 
     def create_block(self):
         raise NotImplementedError()
