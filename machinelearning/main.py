7972a7f1632e4bf9429c8c8b37e795ef524fbada
Come Vitis <business.vitis@gmail.com>
2022-07-23 16:00:21 +0200

    Scatter plot of coordinates

--- machinelearning/main.py
+++ machinelearning/main.py
@@ -4,11 +4,16 @@ import matplotlib.pyplot as plt
 from lib import kmeans
 from lib import utils
 
-def plot_points(points):
+def scatter_plot_coordinates(points):
 
-    plt.plot(points)
+    x_coordinates = list(map(lambda c: c[0], points))
+    y_coordinates = list(map(lambda c: c[1], points))
+
+
+    plt.scatter(x_coordinates, y_coordinates)
+    plt.show()
 
 if __name__ == "__main__":
     """  """
-    points = utils.generate_random_2d_clusters(100, 2)
-    plot_points(points)
+    points = utils.generate_random_2d_clusters(100, 3, 6)
+    scatter_plot_coordinates(points)
