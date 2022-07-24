219fa44dc5d4f263589f4ec983b462c90dd71577
Come Vitis <business.vitis@gmail.com>
2022-07-24 13:34:07 +0200

    First non optimized version of kmeans

--- machinelearning/lib/kmeans.py
+++ machinelearning/lib/kmeans.py
@@ -1,18 +1,55 @@
+import math
+import numpy as np
+
 def euclidian_distance(u, v):
     """ Compute distance between two points. """
-    
+    return np.sqrt((u[0] - v[0])**2 + (u[1] - v[1])**2)
 
 def cost(points, clusters):
     """ Cost is sum of distances for each point to the centroid of their cluster """
 
+def compute_centroid(points):
+    """ Given points, compute the centroid. """
+    x_coordinates = list(map(lambda c: c[0], points))
+    y_coordinates = list(map(lambda c: c[1], points))
+
+    return [np.mean(x_coordinates), np.mean(y_coordinates)]
+
+def shorted_centroid(point, centroids):
+    min_dist = +np.Inf
+    nearest = None
+    for centroid in centroids:
+        curr_dist = euclidian_distance(centroid, point)
+        if curr_dist < min_dist:
+            nearest = centroid
+            min_dist = curr_dist
+    return nearest
+
 def kmeans(points, number_cluster=3, max_iter=100):
     """ Self-made K-means algorithm """
     # Init clusters with random centroids
+    centroid_indexes = np.random.randint(0, 50, size=number_cluster)
+    centroids = points[centroid_indexes]
+    clusters = {}
 
     # Till convergence or max_iter
+    while max_iter < 100:
+        max_iter -= 1
+        
+        # Reset clusters
+        clusters = {} 
+        for centroid in clusters:
+            clusters[str(centroid)] = []
 
         # Give each point a cluster (nearest centroid)
+        for point in points:
+            nearest_centroid = shorted_centroid(point, centroids)
+            clusters[str(nearest_centroid)].append(point)
 
         # Compute for each cluster the new centroid
-
-        # Get score
+        new_centroids = []
+        for _, value in clusters.items():
+            new_centroids.append(compute_centroid(value))
+        centroids = new_centroids
+    
+    return clusters, centroids
--- machinelearning/main.py
+++ machinelearning/main.py
@@ -7,5 +7,10 @@ from lib import utils
 if __name__ == "__main__":
     """  """
     points = utils.generate_random_2d_clusters(100, 3, 6)
-    print(len(points))
-    utils.scatter_plot_coordinates(points)
+
+    print(kmeans.kmeans(points))
+
+    #utils.scatter_plot_coordinates(points)
+
+    #print(kmeans.euclidian_distance([0, 0], [1, 1]))
+    #print(kmeans.compute_centroid(points))
