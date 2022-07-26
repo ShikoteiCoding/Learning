6a52fe9b9abd321fa27aed1128dba6d91d4aac19
Come Vitis <business.vitis@gmail.com>
2022-07-26 17:32:44 +0200

    Setup for pca algorithm

---
+++ machinelearning/lib/pca.py
@@ -0,0 +1,13 @@
+
+
+def pca(dataset):
+    """ Custom simple PCA algorithm """
+
+    # Standardization
+
+    # Covariance matrix
+
+    # Eigen vectors + Eigen values of covariance matrix
+
+    # Feature vector
+ 
--- machinelearning/main.py
+++ machinelearning/main.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 
 from lib import kmeans
 from lib import utils
+from lib import pca
 
-if __name__ == "__main__":
-    """  """
+def demo_kmeans():
     points = utils.generate_random_2d_clusters(100, 3, 5)
     points_to_cluster, centroids = kmeans.kmeans(points)
     utils.scatter_plot_coordinates(points, points_to_cluster)
+
+if __name__ == "__main__":
+    """  """
