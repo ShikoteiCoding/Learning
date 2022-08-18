983a00041c47105bf6e2f9b4faa22d060d8030fb
Come Vitis <business.vitis@gmail.com>
2022-08-18 04:21:14 +0200

    Queue

--- datastructures/arrays.py
+++ datastructures/arrays.py
@@ -17,3 +17,23 @@ class Stack:
 
     def __str__(self):
         return str(self.__container)
+
+
+class Queue:
+    """ FIFO """
+
+    def __init__(self):
+        self.__container = []
+
+    def add(self, el):
+        self.__container.append(el)
+
+    def remove(self):
+        if len(self.__container) == 0:
+            raise IndexError("Queue is empty.")
+        fi = self.__container[0]
+        self.__container = self.__container[1:]
+        return fi
+
+    def __str__(self):
+        return str(self.__container)
--- datastructures/main.py
+++ datastructures/main.py
@@ -1,7 +1,7 @@
-from arrays import Stack
+from arrays import Stack, Queue
 
 
-def test_queue():
+def test_stack():
     s = Stack()
     s.add(1)
     s.add(2)
@@ -13,7 +13,28 @@ def test_queue():
     s.remove()
     s.remove()
     print(f"After removing everything: {s}")
-    s.remove()
+    try:
+        s.remove()
+    except:
+        print("Empty Stack")
+
+def test_queue():
+    q = Queue()
+
+    q.add(1)
+    q.add(2)
+    q.add(3)
+
+    print(f"After adding 3 elements: {q}")
+    removed = q.remove()
+    print(f"After removing one element: {q}, removed element is {removed}")
+    q.remove()
+    q.remove()
+    print(f"After removing everything: {q}")
+    try:
+        q.remove()
+    except:
+        print("Empty Queue")
 
 if __name__ == "__main__":
     print("Examples go here")
