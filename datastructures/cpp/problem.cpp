859b9eac89fc27bd6d15de452a96d4d998b5ce20
Come Vitis <business.vitis@gmail.com>
2022-09-05 08:32:48 +0200

    cpp is garbage for no reason

--- datastructures/cpp/problem.cpp
+++ datastructures/cpp/problem.cpp
@@ -1,7 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
-    public:
+
+public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // 
         vector<int> seenIndices;
         return seenIndices;
     }
+
 };
+
+int main() {
+
+    vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(7);
+    nums.push_back(11);
+    nums.push_back(15);
+
+    Solution s;
+
+    vector<int> output = s.twoSum(nums, 9);
+
+    for (int i = 0; i < 4; i++) {
+        cout << nums[i] << ' ';
+    }
+
+    return 0;
+}
